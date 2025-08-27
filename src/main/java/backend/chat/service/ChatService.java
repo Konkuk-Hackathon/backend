6f1f57d2f8eca26928dadb6541dc93798a8fc13e
jay <rlaakswo0687@gmail.com>
2025-08-27 18:59:11 +0900

    채팅 조회 API 개발 완료

--- src/main/java/backend/chat/service/ChatService.java
+++ src/main/java/backend/chat/service/ChatService.java
@@ -1,7 +1,9 @@
 package backend.chat.service;
 
+import backend.chat.repository.ChatRepository;
 import backend.chat.repository.MemberRepository;
 import backend.chat.repository.ThreadRepository;
+import backend.domain.Chat;
 import backend.domain.Member;
 import backend.domain.Thread;
 import lombok.RequiredArgsConstructor;
@@ -11,18 +13,20 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import static org.springframework.ai.chat.memory.ChatMemory.CONVERSATION_ID;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 
 @Service
 @RequiredArgsConstructor
 
-public class ChatService implements ChatUseCase{
+class ChatService implements ChatUseCase{
 
     private final ChatClient chatClient;
     private final PromptBuilder promptBuilder;
     private final ThreadRepository threadRepository;
     private final MemberRepository memberRepository;
+    private final ChatRepository chatRepository;
 
     @Override
     public Thread findThread(Member member, LocalDateTime chatSentTime) {
@@ -44,16 +48,28 @@ public class ChatService implements ChatUseCase{
     @Override
     public void updateThread(Member member, Thread thread, LocalDateTime chatSentTime) {
         memberRepository.save(member);  // Thread 를 처음 생성하는 경우를 고려해서 member를 영속성 컨텍스트에 추가
-        threadRepository.save(thread);
         thread.updateLastActivityTime(chatSentTime);
+        threadRepository.save(thread);
     }
 
     private Thread createThread(LocalDateTime createdTime, Member member){
         return Thread.builder()
                 .conversationId(UUID.randomUUID().toString())
                 .createdTime(createdTime)
+                .lastActivityTime(createdTime)
                 .member(member)
                 .isActive(true)
                 .build();
     }
+
+    @Override
+    public List<Thread> findThreadsOfMember(Member member) {
+        return threadRepository.findAllByMember(member);
+    }
+
+    @Override
+    public List<Chat> findChatsOfThread(Thread thread) {
+        return chatRepository.findByConversationId(thread.getConversationId());
+    }
+
 }
--- src/main/java/backend/chat/service/ChatUseCase.java
+++ src/main/java/backend/chat/service/ChatUseCase.java
@@ -1,10 +1,12 @@
 package backend.chat.service;
 
 
+import backend.domain.Chat;
 import backend.domain.Member;
 import backend.domain.Thread;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public interface ChatUseCase {
 
@@ -13,6 +15,10 @@ public interface ChatUseCase {
     String sendChat(String conversationId, String message, Guest guest);
 
     void updateThread(Member member, Thread thread, LocalDateTime chatSentTime);
+
+    List<Thread> findThreadsOfMember(Member member);
+
+    List<Chat> findChatsOfThread(Thread thread);
 
 
 }
